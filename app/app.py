011350b8cfdcc99b258887ec2b283346651200c9
Patrick Higgins <phiggins@cs.uoregon.edu>
2022-08-19 05:50:21 -0500

    fix: ips

--- app/app.py
+++ app/app.py
@@ -4,9 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if request.headers.getlist("X-Forwarded-For"):
-        ip = request.headers.getlist("X-Forwarded-For")[0]
-    else:
-        ip = request.remote_addr
+    ip = request.access_route[0]
 
     return render_template("index.html", visitor_ip=ip)
