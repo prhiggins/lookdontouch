9c9d41a3ac59bea0bf7e592f362ae241e0ebb434
Patrick Higgins <phiggins@cs.uoregon.edu>
2022-08-19 05:44:45 -0500

    fix proxy ip hiding

--- app/app.py
+++ app/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    ip = request.remote_addr
+def index():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
     return render_template("index.html", visitor_ip=ip)
