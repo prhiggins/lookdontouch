99ae60841824e8ec46ce0508929603962b1c76e2
Patrick Higgins <phiggins@cs.uoregon.edu>
2022-08-19 19:02:56 -0500

    doc: docstrings

--- app/visitor_context.py
+++ app/visitor_context.py
@@ -2,7 +2,31 @@ from dataclasses import dataclass
 
 @dataclass
 class VisitorContext:
-    ip_address: str = None
-    cookies: list[str] = None
+    '''
+    Provides the context for a page display. Contains data scraped from the visitor's client.
+    '''
+
+    remote_address: str = None
+    '''
+    The remote address of the visitor's client.
+    '''
+
+    addr_chain: list[str] = remote_address
+    '''
+    The entire chain of addresses, in order, gathered from the contents of the `X-Forwarded-By` header.
+    '''
+
+    scraped_cookies: list[str] = None
+    '''
+    The contents of any cookies extracted from the user's client.
+    '''
+    
     user_agent: str = None
+    '''
+    The client user-agent.
+    '''
+
     other_headers: dict[str] = None
+    '''
+    A dictionary keyed with any other headers in the request.
+    '''
